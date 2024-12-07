745638111b1d55749377c0338e962f3557c748a9
Stephen Monnet <stephen-monnet@hotmail.com>
2024-12-07 19:35:43 +0100

    Update setup.py for cython

--- nav_env/ships/setup.py
+++ nav_env/ships/setup.py
@@ -1,13 +1,13 @@
-# from setuptools import setup
-# from Cython.Build import cythonize
+from setuptools import setup
+from Cython.Build import cythonize
 
-# setup(
-#     ext_modules = cythonize("nav_env/ships/physics.pyx")
-# )
+setup(
+    ext_modules = cythonize("nav_env/ships/physics.pyx")
+)
 
 """
 To build the extension module, run the following command in the terminal, from the nav_env (top) directory:
-python setup.py build_ext --inplace
+python nav_env\ships\setup.py build_ext --inplace
 
 Then in other python files, import it using:
 import nav_env.ships.physics as physics
