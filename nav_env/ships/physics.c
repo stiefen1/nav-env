69a246a8e6933ded3a322c342c68ad814e7fefe8
Stephen Monnet <stephen-monnet@hotmail.com>
2025-02-06 14:29:47 +0100

    misc

--- nav_env/ships/physics.c
+++ nav_env/ships/physics.c
@@ -858,7 +858,7 @@ static CYTHON_INLINE void *__Pyx_PyModule_GetState(PyObject *op)
     void *result;
     result = PyModule_GetState(op);
     if (!result)
-        Py_FatalError("Couldn't find the module states");
+        Py_FatalError("Couldn't find the module state");
     return result;
 }
 #endif
